90b996564356beb1a1a9dd130a7e4d7fb2700284
Fangbo <fangbo.tao@gmail.com>
2017-07-18 14:58:22 -0500

    preprocess script to generate phrase candidates

---
+++ code/preprocess.py
@@ -0,0 +1,27 @@
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+
+def get_candidates(folder, o_file):
+
+	files = ['%s%s' % (folder, f) for f in listdir(folder) if isfile(join(folder, f))]
+	quality_phrases = set()
+
+	for file in files:
+		with open(file) as f:
+			for line in f:
+				phrase = line.split(' ')[0]
+				quality_phrases.add(phrase)
+
+	print 'Quality phrase count: ' + str(len(quality_phrases))
+
+	with open(o_file, 'w+') as g:
+		for phrase in quality_phrases:
+			g.write('%s\n' % phrase)
+
+
+folder = '../data/leef/query_relevant/'
+o_file = '../data/candidates.txt'
+
+get_candidates(folder, o_file)
