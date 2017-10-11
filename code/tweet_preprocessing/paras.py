45ce651ba4be8c87335025fde739bfd5ec2d1bbe
lunanli3 <lunanli3@illinois.edu>
2017-10-11 15:54:20 -0500

    generate keywords from pos tag. select only noun

--- code/tweet_preprocessing/paras.py
+++ code/tweet_preprocessing/paras.py
@@ -2,3 +2,6 @@ la_tweets = '/shared/data/lunanli3/local-embedding/input/tweets/la/tweets.txt'
 la_output = '/shared/data/lunanli3/local-embedding/output/tweets/la'
 lexnorm = './../../lib/emnlp2012-lexnorm/emnlp_dict.txt'
 lexnorm_dic = '/shared/data/lunanli3/local-embedding/output/tweets/la/lexnorm.txt'
+la_pos_tweets = '/shared/data/lunanli3/local-embedding/output/tweets/la/tweets_pos_tag.txt'
+la_keywords = '/shared/data/lunanli3/local-embedding/output/tweets/la/keywords.txt'
+
---
+++ code/tweet_preprocessing/seed_term_generator.py
@@ -0,0 +1,43 @@
+from paras import la_pos_tweets, la_keywords
+from datetime import datetime
+
+class KeywordGenerator:
+
+    def __init__(self, f_in, f_out):
+        self.input = f_in
+        self.output = f_out
+        self.keywords = set()
+        self.noun_tag = set(['NN', 'NNS', 'NNP', 'NNPS'])
+
+    def parse_pos_tweet(self, pos_tweet):
+
+        pos_tweet = pos_tweet.split(' ')
+
+        for segment in pos_tweet:
+            segment = segment.split('/')
+
+            if segment[2] in self.noun_tag:
+                self.keywords.add(segment[0])
+
+    def build_keyword(self):
+
+        with open(self.input, 'r') as f:
+            data = f.readlines()
+
+            for pos_tweet in data:
+                self.parse_pos_tweet(pos_tweet)
+
+        with open(self.output, 'w+') as f:
+            f.writelines(self.keywords)
+
+if __name__ == '__main__':
+    start = datetime.utcnow()
+    gen = KeywordGenerator(la_pos_tweets, la_keywords)
+    gen.build_keyword()
+    finish = datetime.utcnow()
+    exec_time = finish-start
+    print exec_time.seconds
+    print exec_time.microseconds
+
+
+
