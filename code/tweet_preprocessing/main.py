fa1899d1d2eb82de825d4f439353e856b9b5e079
lunanli3 <lunanli3@illinois.edu>
2017-10-12 16:25:59 -0500

    fix logger dir error

--- code/tweet_preprocessing/main.py
+++ code/tweet_preprocessing/main.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess, os
 from util.logger import Logger
 from tweet_handler import TweetHandler
 from paras import la_log, la_tweets, la_input, la_pos_tweets, la_keywords, la_pure_tweets, lexnorm, MAIN_LOG
@@ -7,7 +7,11 @@ from seed_term_generator import KeywordGenerator
 
 if __name__ == '__main__':
     git_version = subprocess.Popen('git rev-parse --short HEAD', shell=True, stdout=subprocess.PIPE).communicate()[0]
-    logger = Logger(la_log+git_version.strip()+"/log.txt")
+    dir = la_log + git_version
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+    logger = Logger(dir+"/log.txt")
 
     # generate lexnorm.txt and pure_tweets.txt
     tweet_handler = TweetHandler(la_tweets, la_input, MAIN_LOG, lexnorm)
