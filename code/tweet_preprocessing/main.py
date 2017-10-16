33f6eff04433fd62e6a079a92a34789519401d2b
lunanli3 <lunanli3@illinois.edu>
2017-10-16 00:35:27 -0500

    fix main.py bug keyword generator

--- code/tweet_preprocessing/main.py
+++ code/tweet_preprocessing/main.py
@@ -1,7 +1,7 @@
 import subprocess, os
 from util.logger import Logger
 from tweet_handler import TweetHandler
-from paras import la_log, la_tweets, la_raw, la_pos_tweets, la_keywords, la_pure_tweets, lexnorm, MAIN_LOG
+from paras import la_log, la_tweets, la_raw, la_pos_tweets, la_keywords, la_pure_tweets, lexnorm, MAIN_LOG, la_category_keywords, la_category, la_embeddings
 from seed_term_generator import KeywordGenerator
 
 if __name__ == '__main__':
@@ -12,11 +12,11 @@ if __name__ == '__main__':
 
     logger = Logger(dir + "/log.txt")
 
-    # generate lexnorm.txt and pure_tweets.txt
+    # generate lexnorm.txt, pure_tweets.txt, embeddings.txt
     tweet_handler = TweetHandler(la_tweets, la_raw, MAIN_LOG, lexnorm)
     tweet_handler.preprocess()
 
     # generate pos_tag_tweets.txt and keywords.txt
-    gen = KeywordGenerator(la_pure_tweets, la_pos_tweets, la_keywords, MAIN_LOG)
+    gen = KeywordGenerator(la_pure_tweets, la_pos_tweets, la_keywords, MAIN_LOG, la_category, la_category_keywords, la_embeddings)
     gen.build_pos_tag_tweets()
     gen.build_keyword()
