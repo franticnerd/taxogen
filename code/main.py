e7ae69fb1e1f30483d52488b19257d8909dc2300
Lunan Li <lunanli3@dmserv5.cs.illinois.edu>
2017-10-19 11:32:54 -0500

    create debugging branch

--- code/main.py
+++ code/main.py
@@ -112,7 +112,7 @@ def main(opt):
     level = 0
 
     # our method
-    root_dir = opt['data_dir'] + 'our-l3-0.15/'
+    root_dir = opt['data_dir'] + 'our-tweets/'
     copy_tree(init_dir, root_dir)
     recur(input_dir, root_dir, n_cluster, '*', n_cluster_iter, filter_thre, n_expand, level, True, True)
 
@@ -136,6 +136,10 @@ if __name__ == '__main__':
     # opt = load_toy_params()
     # opt = load_dblp_params()
     # opt = load_sp_params()
-    opt = load_dblp_params_method()
+    #opt = load_dblp_params_method()
+    opt = load_tweets_params_method('tweets/la')
+    print()
+    print("[Main] Finish load parameters: %s" % str(opt))
+
     main(opt)
 
