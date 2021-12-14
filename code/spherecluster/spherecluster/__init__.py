9e231940c94e10257806f31a92f1206dab819b1d
Cezar Sas <cezar.sas@gmail.com>
2021-12-14 09:52:40 +0100

    Minor fixes

--- code/spherecluster/spherecluster/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from .spherical_kmeans import SphericalKMeans
-from .von_mises_fisher_mixture import VonMisesFisherMixture
-from .util import sample_vMF
-
-__all__ = ["SphericalKMeans", "VonMisesFisherMixture", "sample_vMF"]
--- code/spherecluster/spherecluster/tests/test_common.py
+++
@@ -1,6 +0,0 @@
-from sklearn.utils.estimator_checks import check_estimator
-from spherecluster import SphericalKMeans
-
-
-def test_estimator_spherical_k_means():
-    return check_estimator(SphericalKMeans)
